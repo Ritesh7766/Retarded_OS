6b91742bcc6c40a6960df9586116e29882b89b44
Ritesh7766 <riteshsaha214@gmail.com>
2023-10-26 21:18:54 +0530

    [ automate code ]

--- Chapter_2(BIOS)/hello.asm
+++ Chapter_2(BIOS)/hello.asm
@@ -1,14 +1,13 @@
 mov ah, 0x0e
-mov al, 'H'
+mov al, 'A'
 int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+
+loop:
+    inc al
+    int 0x10
+    cmp al, 'Z' - 1
+    jle loop
+
 jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
